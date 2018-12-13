2d70c6c88778979eb722af04b90e2ec8c6430ff6
Stefan Gvozdenovic <gefa12@gmail.com>
2018-12-13 15:01:08 -0500

    edit comment for serial port script

--- wyze_serial.sh
+++ wyze_serial.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# This script finds first serial device (e.g. /dev/ttyUSB0) and runs minicom on it
 # might need to turn off hardware flow-control if running for the first time
-# run sudo minicom; press CTRL-A; press z; press o; goto serial port setup; press f;
+# run sudo minicom; press CTRL-A; press z; press o; goto serial_port_setup; press f;
+# press enter; save setup as dfl; press exit; CTRL-A; z; x; enter; rerun this script 
 export SERIAL=`echo -n "/dev/";dmesg |grep tty|grep USB|rev|awk '{print $1;exit}'|rev`
 DATE=$(date +"%Y_%m_%d_%H%M")
 sudo minicom -b 115200 -8 $SERIAL -C $DATE.txt
